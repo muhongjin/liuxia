79fa2e266f3b3f14d5861bf65d922b5c7fa24170
muhongjin <93109615+muhongjin@users.noreply.github.com>
2022-03-03 16:50:17 +0800

    Delete historysystem directory

--- historysystem/src/main/java/com/example/remark/service/CreateService.java
+++
@@ -1,7 +0,0 @@
-package com.example.remark.service;
-import com.example.remark.utils.ResultBody;
-public interface CreateService {
-    ResultBody create(String name);
-    ResultBody drop(String name);
-    ResultBody exist(String name);
-}
--- historysystem/src/main/java/com/example/remark/service/HistoryService.java
+++
@@ -1,7 +0,0 @@
-package com.example.remark.service;
-import com.example.remark.utils.ResultBody;
-public interface HistoryService {
-    ResultBody addHistory(String name,double time);
-    ResultBody getAll(String name);
-    ResultBody getNum(String name);
-}
